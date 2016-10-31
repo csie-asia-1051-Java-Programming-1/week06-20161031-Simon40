e1c2e1f830985432795290e15e6550cee955dfef
andy20277703 <105021060@live.asia.edu.tw>
2016-10-31 16:15:18 +0800

    ex上傳1

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -5,14 +5,28 @@
  @@@
  @@@
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021060 陳俊仁
  */
-
+import java.util.Scanner; 
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        String m=scn.next();
+        fun1(n,m);
 	}
-
+	public static int fun1(int v1,String v2){
+    	int sum=0;
+    	for(int i=1;i<=v1;i++){
+    		for(int j=1;j<=v1;j++){
+    			System.out.print(v2);
+    			
+    		}
+    		System.out.println();
+    	}
+		return sum;
+    	
+	}
 }
